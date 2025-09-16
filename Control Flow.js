2895cd40a2badc1e44da3be0079b63e882a5f61a
PankajJadhav18 <pankajjadhavfsd@gmail.com>
2025-09-16 23:26:22 +0530

    Add files via upload

---
+++ Control Flow.js
@@ -0,0 +1,17 @@
+let marks = 85;
+
+if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks >= 75) {
+    console.log("Grade B");
+} else {
+    console.log("Grade C");
+}
+
+// Swich case
+let day = 2;
+switch (day) {
+    case 1: console.log("Monday"); break;
+    case 2: console.log("Tuesday"); break;
+    default: console.log("Other Day");
+}
