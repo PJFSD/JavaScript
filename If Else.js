30f6f53f367ca7bd1e685c2dac2620209dd4dc3d
PankajJadhav18 <pankajjadhavfsd@gmail.com>
2026-07-21 10:33:42 +0530

    Add files via upload

---
+++ If Else.js
@@ -0,0 +1,15 @@
+// let marks = 80;
+
+// if (marks > 70) {
+//     console.log("Bike");
+// } else {
+//     console.log("No Bike");
+// }
+
+let price = 1000;
+
+if (price > 970) {
+    console.log("buy");
+} else {
+    console.log("Not Buy");
+}
